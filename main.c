49b4fe531d8686f7ff932e5c3ff1f456aa1d4a94
ArthurdaSilvaMota <arthurdasilvamota77@gmail.com>
2026-08-06 11:32:44 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char *argv[]) {
+	
+	float a,b,c;
+	
+	a = 8;
+	b = 19;
+	c = a/b;
+	c = a+b;
+	c = a-b;
+	c = a*b;
+	
+	printf ("a soma de %f ? %f = %f", a,b,c);
+	
+	return 0;
+}
