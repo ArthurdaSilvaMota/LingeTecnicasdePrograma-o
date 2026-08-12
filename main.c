a8c51ac40d7309d6e2ed7c7a008dbe906a297e7a
ArthurdaSilvaMota <arthurdasilvamota77@gmail.com>
2026-08-12 11:42:29 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,19 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define pi 3.141592
 
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
 	
-	float a,b,c;
 	
-	a = 8;
-	b = 19;
-	c = a/b;
-	c = a+b;
-	c = a-b;
-	c = a*b;
+    float area, raio, r2;
+    
+    printf("Insira o raio do circulo ");
+    scanf("%f", &raio);
+    
+	r2 = raio*raio;
+	//raio *= raio; esse é um outro jeito de fazer "raio vezes raio" 
+	area = pi*r2;
 	
-	printf ("a soma de %f ? %f = %f", a,b,c);
+	printf("A Area do circulo de raio %f = %f", raio, area);
+	
+	
+	
+	printf("Agora a do trapezio ");
+	
+	float basemaior, basemenor, h;
+	
+	printf("insira a base maior ");
+	scanf("%f", &basemaior);
+	
+	printf("insira a base menor ");
+	scanf("%f", &basemenor);
+	
+	printf("insira altura ");
+	scanf("%f", &h);
+	
+	area = (basemaior + basemenor)*h /2;
+	printf("A area do trapezio %f = %f", basemaior, basemenor, h);
 	
 	return 0;
 }
