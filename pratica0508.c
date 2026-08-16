c8b5ae208e6eb63afe5b1a9b3ba906ee6981623e
ArthurdaSilvaMota <arthurdasilvamota77@gmail.com>
2026-08-16 19:13:27 -0300

    Add files via upload

---
+++ pratica0508.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main(int argc, char *argv[]) {
+	printf("que saudades do cidao");// esse "//" é tipo um "comentário", ele não afeta o código, e pode ser usado para anotar coisas (mas em linhas)
+	return 0;
+}
+
+"/*" faz a mesma coisa, mas no bloco inteiro
